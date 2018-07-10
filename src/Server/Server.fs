8dccb3501d36581f42db138bd597d07d0c58fd2d
Johannes Egger <j.egger@posteo.at>
2018-07-10 22:15:57 +0200

    Use WakeOnLan NuGet package to send WoL magic packet

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -1,30 +1,59 @@
 open System.IO
-open System.Threading.Tasks
 
-open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 open Saturn
-open Shared
 
 open Giraffe.Serialization
-open System.Diagnostics
+open System.Net
+open System.Net.Sockets
+open System.Net.NetworkInformation
 
 let publicPath = Path.GetFullPath "../Client/public"
 let port = 8085us
 
-let sendWakeUpCommand() = task {
-    let p = Process.Start("wakeonlan", "20:CF:30:81:37:03")
-    p.WaitForExit()
-    if p.ExitCode <> 0 then ()
-    else failwith "Error while running `wakeonlan`"
+type WakeUpError =
+    | HostResolutionError of exn
+    | GetIpAddressFromHostNameError of IPAddress list
+    | WakeOnLanError of exn
+
+let sendWakeUpCommand (hostName: string) (macAddress: PhysicalAddress) = Result.result {
+    let! hostEntry =
+        try
+            Dns.GetHostEntry hostName
+            |> Ok
+        with e ->
+            HostResolutionError e
+            |> Error
+            
+    let! ipAddress =
+        hostEntry.AddressList
+        |> Seq.tryFind (fun p -> p.AddressFamily = AddressFamily.InterNetwork)
+        |> Option.orElse (Seq.tryHead hostEntry.AddressList)
+        |> Result.ofOption (GetIpAddressFromHostNameError (List.ofArray hostEntry.AddressList))
+
+    return!
+        try
+            ipAddress.SendWol macAddress
+            Ok ()
+        with e ->
+            WakeOnLanError e
+            |> Error
 }
 
 let webApp = scope {
     get "/api/send-wakeup-command" (fun next ctx ->
         task {
-            do! sendWakeUpCommand()
-            return! Successful.OK () next ctx
+            let result = sendWakeUpCommand "pc-eggj" (PhysicalAddress([| 0x20uy; 0xCFuy; 0x30uy; 0x81uy; 0x37uy; 0x03uy |]))
+            return!
+                match result with
+                | Ok () -> Successful.OK () next ctx
+                | Error (HostResolutionError e) ->
+                    ServerErrors.INTERNAL_ERROR (sprintf "Error while resolving host name: %O" e) next ctx
+                | Error (GetIpAddressFromHostNameError addresses) ->
+                    ServerErrors.INTERNAL_ERROR (sprintf "Error while getting IP address from host name. Address candidates: %A" addresses) next ctx
+                | Error (WakeOnLanError e) ->
+                    ServerErrors.INTERNAL_ERROR (sprintf "Error while sending WoL magic packet: %O" e) next ctx
         })
 }
 
