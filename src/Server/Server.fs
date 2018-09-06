506ccbe702a0453dbf6858c81535d27544c83b01
Johannes Egger <j.egger@posteo.at>
2018-09-06 20:16:27 +0200

    Extract WakeUp.fs

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -1,50 +1,19 @@
 open System.IO
 
+open System.Net.NetworkInformation
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
-open Saturn
-
 open Giraffe.Serialization
-open System.Net
-open System.Net.Sockets
-open System.Net.NetworkInformation
+open Saturn
+open WakeUp
 
 let publicPath = Path.GetFullPath "../Client/public"
 let port = 8085us
 
-type WakeUpError =
-    | HostResolutionError of string
-    | GetIpAddressFromHostNameError of string * IPAddress list
-    | WakeOnLanError of IPAddress * PhysicalAddress
-
-let sendWakeUpCommand (hostName: string) (macAddress: PhysicalAddress) = Result.result {
-    let! hostEntry =
-        try
-            Dns.GetHostEntry hostName
-            |> Ok
-        with e ->
-            HostResolutionError hostName
-            |> Error
-
-    let! ipAddress =
-        hostEntry.AddressList
-        |> Seq.tryFind (fun p -> p.AddressFamily = AddressFamily.InterNetwork)
-        |> Option.orElse (Seq.tryHead hostEntry.AddressList)
-        |> Result.ofOption (GetIpAddressFromHostNameError (hostName, List.ofArray hostEntry.AddressList))
-
-    return!
-        try
-            ipAddress.SendWol macAddress
-            Ok ()
-        with e ->
-            WakeOnLanError (ipAddress, macAddress)
-            |> Error
-}
-
 let webApp = scope {
     get "/api/send-wakeup-command" (fun next ctx ->
         task {
-            let result = sendWakeUpCommand "pc-eggj" (PhysicalAddress([| 0x20uy; 0xCFuy; 0x30uy; 0x81uy; 0x37uy; 0x03uy |]))
+            let result = WakeUp.sendWakeUpCommand "pc-eggj" (PhysicalAddress.Parse "20-CF-30-81-37-03")
             return!
                 match result with
                 | Ok () -> Successful.OK () next ctx
