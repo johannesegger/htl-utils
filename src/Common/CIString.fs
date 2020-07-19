a94508fe7067981b46806816e70267003e64cc69
johannesegger <j.egger@posteo.at>
2020-07-19 21:58:37 +0200

    Simplify CIString

--- src/Common/CIString.fs
+++ src/Common/CIString.fs
@@ -1,7 +1,6 @@
 namespace global
 
 open System
-open System.Globalization
 
 [<CustomEquality; CustomComparison>]
 type CIString =
@@ -19,8 +18,7 @@ type CIString =
 
         override this.GetHashCode() =
             let (CIString v) = this
-            CultureInfo.InvariantCulture.CompareInfo.GetSortKey(v, CompareOptions.IgnoreCase).KeyData
-            |> Array.fold (curry HashCode.Combine) 0
+            StringComparer.InvariantCultureIgnoreCase.GetHashCode(v)
 
         interface System.IComparable with
             member this.CompareTo other =
@@ -28,5 +26,5 @@ type CIString =
                 | :? CIString as other ->
                     let (CIString x) = this
                     let (CIString y) = other
-                    String.Compare(x, y, StringComparison.InvariantCultureIgnoreCase)
+                    StringComparer.InvariantCultureIgnoreCase.Compare(x, y)
                 | _ -> invalidArg "other" "Cannot compare objects of different types"
