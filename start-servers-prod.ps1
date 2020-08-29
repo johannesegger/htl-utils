89c77138bb7c0e9dd728fa19119153fb5583a3d9
johannesegger <j.egger@posteo.at>
2020-08-29 10:27:59 +0200

    Fix starting production servers

--- start-servers-prod.ps1
+++ start-servers-prod.ps1
@@ -1,9 +1,11 @@
-dotnet publish .\src\Teaching.Server -c Release -o "$PSScriptRoot\deploy\Teaching"
-dotnet publish .\src\Management.Server -c Release -o "$PSScriptRoot\deploy\Management"
+$teachingDir = "$PSScriptRoot\deploy\Teaching"
+# dotnet publish .\src\Teaching.Server -c Release -o $teachingDir
+# yarn --cwd .\src\Teaching.Client webpack --output-path "$teachingDir\wwwroot"
 
-yarn --cwd .\src\Teaching.Client webpack --output-path "$PSScriptRoot\deploy\Teaching\wwwroot"
-yarn --cwd .\src\Management.Client webpack --output-path "$PSScriptRoot\deploy\Management\wwwroot"
+$managementDir = "$PSScriptRoot\deploy\Management"
+# dotnet publish .\src\Management.Server -c Release -o $managementDir
+# yarn --cwd .\src\Management.Client webpack --output-path "$managementDir\wwwroot"
 
 wt `
-    new-tab --title Teaching --startingDirectory .\deploy\Teaching `-`- .\Teaching.Server.exe --urls=http://+:3000 `; `
-    new-tab --title Management --startingDirectory .\deploy\Management `-`- .\Management.Server.exe --urls=http://+:3001 `
+    new-tab --title Teaching --startingDirectory $teachingDir `-`- "$teachingDir\Teaching.Server.exe" --urls=http://+:3000 `; `
+    new-tab --title Management --startingDirectory $managementDir `-`- "$managementDir\Management.Server.exe" --urls=http://+:3001
