d899b0785de86bc843438e764c3ac30226df764b
johannesegger <j.egger@posteo.at>
2020-08-30 21:03:47 +0200

    Build before starting production servers

--- start-servers-prod.ps1
+++ start-servers-prod.ps1
@@ -1,10 +1,10 @@
 $teachingDir = "$PSScriptRoot\deploy\Teaching"
-# dotnet publish .\src\Teaching.Server -c Release -o $teachingDir
-# yarn --cwd .\src\Teaching.Client webpack --output-path "$teachingDir\wwwroot"
+dotnet publish .\src\Teaching.Server -c Release -o $teachingDir
+yarn --cwd .\src\Teaching.Client webpack --output-path "$teachingDir\wwwroot"
 
 $managementDir = "$PSScriptRoot\deploy\Management"
-# dotnet publish .\src\Management.Server -c Release -o $managementDir
-# yarn --cwd .\src\Management.Client webpack --output-path "$managementDir\wwwroot"
+dotnet publish .\src\Management.Server -c Release -o $managementDir
+yarn --cwd .\src\Management.Client webpack --output-path "$managementDir\wwwroot"
 
 wt `
     new-tab --title Teaching --startingDirectory $teachingDir `-`- "$teachingDir\Teaching.Server.exe" --urls=http://+:3000 `; `
