51f7d7c06a30140893ff1ae287c26555a0641a12
johannesegger <j.egger@posteo.at>
2020-08-30 20:53:33 +0200

    Fix `HttpHandler.nil`

--- src/Common.Server/HttpHandler.fs
+++ src/Common.Server/HttpHandler.fs
@@ -1,5 +1,6 @@
 module HttpHandler
 
+open FSharp.Control.Tasks.V2.ContextInsensitive
 open Giraffe.Core
 
-let nil : HttpHandler = fun next ctx -> next ctx
+let nil : HttpHandler = fun next ctx -> task { return Some ctx }
