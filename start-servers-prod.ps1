1ac9640f984327a1ce5e9aa80f644df54e06d667
johannesegger <j.egger@posteo.at>
2020-08-30 21:15:38 +0200

    Restore npm packages before building production servers

--- start-servers-prod.ps1
+++ start-servers-prod.ps1
@@ -1,9 +1,11 @@
 $teachingDir = "$PSScriptRoot\deploy\Teaching"
 dotnet publish .\src\Teaching.Server -c Release -o $teachingDir
+yarn --cwd .\src\Teaching.Client install --frozen-lockfile
 yarn --cwd .\src\Teaching.Client webpack --output-path "$teachingDir\wwwroot"
 
 $managementDir = "$PSScriptRoot\deploy\Management"
 dotnet publish .\src\Management.Server -c Release -o $managementDir
+yarn --cwd .\src\Management.Client install --frozen-lockfile
 yarn --cwd .\src\Management.Client webpack --output-path "$managementDir\wwwroot"
 
 wt `
