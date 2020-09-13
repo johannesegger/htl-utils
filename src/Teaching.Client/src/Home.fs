f9f9fe21b7a52ddc4d6674a25777f8ff16379157
johannesegger <j.egger@posteo.at>
2020-09-13 13:58:49 +0200

    Resize home tiles of teaching app

--- src/Teaching.Client/src/Home.fs
+++ src/Teaching.Client/src/Home.fs
@@ -8,12 +8,12 @@ open Pages
 let view =
     let tiles = [
         [
-            WakeUp, Tile.Is3, "Wake up", str "Send a Wake-on-Lan \"magic packet\" to a computer by specifying its MAC address."
+            WakeUp, Tile.Is4, "Wake up", str "Send a Wake-on-Lan \"magic packet\" to a computer by specifying its MAC address."
             AddAADTeacherContacts, Tile.Is4, "Add teacher contacts", str "Add teachers as Outlook contacts with photo, contact information and more."
             KnowName, Tile.Is3, "Know name", str "Learn teacher and student names given their photos."
         ]
         [
-            CreateStudentDirectories, Tile.Is4, "Create student directories", str "Create a directory per student for exercises, tests, etc."
+            CreateStudentDirectories, Tile.Is5, "Create student directories", str "Create a directory per student for exercises, tests, etc."
             CreateStudentGroups, Tile.Is4, "Create student groups", str "Create random groups of students for team work, seating plans, ordering and more."
             InspectDirectory, Tile.Is3, "Inspect directory", str "Get information about a directory and its files."
         ]
