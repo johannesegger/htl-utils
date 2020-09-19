5f8e89147badf03a3be0695dd68a91b9704289b2
johannesegger <j.egger@posteo.at>
2020-09-19 18:30:23 +0200

    Add ClassList util

---
+++ utils/ClassList/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let className =
+        argv
+        |> Array.tryItem 0
+        |> Option.defaultWith (fun () ->
+            printf "Class: "
+            Console.ReadLine()
+        )
+    let students = Sokrates.Core.getStudents (Some className) None |> Async.RunSynchronously
+    students
+    |> List.sortBy (fun student -> student.LastName, student.FirstName1, student.FirstName2)
+    |> List.iter (fun student ->
+        printfn "%s %s %s"
+            student.LastName
+            student.FirstName1
+            (Option.defaultValue "" student.FirstName2)
+    )
+    0
