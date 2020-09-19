460354a3d892965118beb61bab1eed6c75688620
johannesegger <j.egger@posteo.at>
2020-09-19 06:57:58 +0200

    Update PreparePhotos to work with non-microservices

--- utils/PreparePhotos/Program.fs
+++ utils/PreparePhotos/Program.fs
@@ -1,5 +1,4 @@
-﻿open System
-open System.IO
+﻿open System.IO
 open System.Net
 open System.Net.Http
 open Thoth.Json.Net
@@ -24,54 +23,46 @@ module Http =
     }
 
 let prepareTeacherPhotos baseDir = async {
-    let! sokratesTeacherResponse = Http.get "http://localhost:3001/api/teachers" (Decode.list Sokrates.DataTransferTypes.Teacher.decoder)
-    match sokratesTeacherResponse with
-    | Ok teachers ->
-        let teacherMap = teachers |> List.map (fun t -> sprintf "%s_%s" t.LastName t.FirstName |> CIString, t.ShortName) |> Map.ofList
-        Directory.GetFiles baseDir
-        |> Seq.choose (fun file ->
-            let fileName = Path.GetFileName file
-            let teacherName = Path.GetFileNameWithoutExtension file
-            let fileExtension = Path.GetExtension(file).ToLowerInvariant()
-            Map.tryFind (CIString teacherName) teacherMap
-            |> Option.map (fun teacherShortName -> fileName, sprintf "%s%s" teacherShortName fileExtension)
-        )
-        |> Seq.iter (fun (source, destination) ->
-            printfn "%s -> %s" source destination
-            File.Move(Path.Combine(baseDir, source), Path.Combine(baseDir, destination))
-        )
-    | Error (Http.HttpError (url, statusCode, content)) -> eprintfn "Can't load teachers from %s (%O)" url statusCode
-    | Error (Http.DecodeError (url, message)) -> eprintfn "Can't decode teachers: %s" message
+    let! teachers = Sokrates.Core.getTeachers ()
+    let teacherMap = teachers |> List.map (fun t -> sprintf "%s_%s" t.LastName t.FirstName |> CIString, t.ShortName) |> Map.ofList
+    Directory.GetFiles baseDir
+    |> Seq.choose (fun file ->
+        let fileName = Path.GetFileName file
+        let teacherName = Path.GetFileNameWithoutExtension file
+        let fileExtension = Path.GetExtension(file).ToLowerInvariant()
+        Map.tryFind (CIString teacherName) teacherMap
+        |> Option.map (fun teacherShortName -> fileName, sprintf "%s%s" teacherShortName fileExtension)
+    )
+    |> Seq.iter (fun (source, destination) ->
+        printfn "%s -> %s" source destination
+        File.Move(Path.Combine(baseDir, source), Path.Combine(baseDir, destination))
+    )
 }
 
 let prepareStudentPhotos baseDir = async {
-    let! sokratesStudentResponse = Http.get "http://localhost:3001/api/students" (Decode.list Sokrates.DataTransferTypes.Student.decoder)
-    match sokratesStudentResponse with
-    | Ok students ->
-        let studentMap =
-            students
-            |> List.map (fun s ->
-                let key = (CIString s.SchoolClass, CIString (sprintf "%s_%s" s.LastName s.FirstName1))
-                (key, s.Id)
-            )
-            |> Map.ofList
-        Directory.GetFiles(baseDir, "*", SearchOption.AllDirectories)
-        |> Seq.choose (fun file ->
-            let studentClass = Path.GetDirectoryName file |> Path.GetFileName
-            let fileName = Path.GetFileName file
-            let studentName = Path.GetFileNameWithoutExtension file
-            let fileExtension = Path.GetExtension(file)
-            Map.tryFind (CIString studentClass, CIString studentName) studentMap
-            |> Option.map (fun (Sokrates.DataTransferTypes.SokratesId studentId) ->
-                Path.Combine(studentClass, fileName), sprintf "%s%s" studentId fileExtension
-            )
+    let! students = Sokrates.Core.getStudents None None
+    let studentMap =
+        students
+        |> List.map (fun s ->
+            let key = (CIString s.SchoolClass, CIString (sprintf "%s_%s" s.LastName s.FirstName1))
+            (key, s.Id)
         )
-        |> Seq.iter (fun (source, destination) ->
-            printfn "%s -> %s" source destination
-            File.Move(Path.Combine(baseDir, source), Path.Combine(baseDir, destination))
+        |> Map.ofList
+    Directory.GetFiles(baseDir, "*", SearchOption.AllDirectories)
+    |> Seq.choose (fun file ->
+        let studentClass = Path.GetDirectoryName file |> Path.GetFileName
+        let fileName = Path.GetFileName file
+        let studentName = Path.GetFileNameWithoutExtension file
+        let fileExtension = Path.GetExtension(file)
+        Map.tryFind (CIString studentClass, CIString studentName) studentMap
+        |> Option.map (fun (Sokrates.Domain.SokratesId studentId) ->
+            Path.Combine(studentClass, fileName), sprintf "%s%s" studentId fileExtension
         )
-    | Error (Http.HttpError (url, statusCode, content)) -> eprintfn "Can't load teachers from %s (%O)" url statusCode
-    | Error (Http.DecodeError (url, message)) -> eprintfn "Can't decode teachers: %s" message
+    )
+    |> Seq.iter (fun (source, destination) ->
+        printfn "%s -> %s" source destination
+        File.Move(Path.Combine(baseDir, source), Path.Combine(baseDir, destination))
+    )
 }
 
 [<EntryPoint>]
