d718587c3a76db7e0ddb0779b50f297ca270272c
johannesegger <j.egger@posteo.at>
2020-09-22 17:10:14 +0200

    Ignore DirectoryNotFoundException when deleting directory

--- src/Common/Directory.fs
+++ src/Common/Directory.fs
@@ -3,10 +3,10 @@ module Directory
 open System.IO
 
 let delete path =
-    // Remove read-only attributes of files and folders
-    Directory.GetDirectories(path, "*", SearchOption.AllDirectories) |> Seq.iter (fun path -> File.SetAttributes(path, FileAttributes.Normal))
-    Directory.GetFiles(path, "*", SearchOption.AllDirectories) |> Seq.iter (fun path -> File.SetAttributes(path, FileAttributes.Normal))
-
     try
+        // Remove read-only attributes of files and folders
+        Directory.GetDirectories(path, "*", SearchOption.AllDirectories) |> Seq.iter (fun path -> File.SetAttributes(path, FileAttributes.Normal))
+        Directory.GetFiles(path, "*", SearchOption.AllDirectories) |> Seq.iter (fun path -> File.SetAttributes(path, FileAttributes.Normal))
+
         Directory.Delete(path, true)
     with :? DirectoryNotFoundException -> ()
