93f274881d3d823148fa65ee043605de4059ffd9
johannesegger <j.egger@posteo.at>
2020-09-22 16:32:19 +0200

    Improve `Directory.delete` by deleting all files first and then the empty directories

--- src/Common/Directory.fs
+++ src/Common/Directory.fs
@@ -2,14 +2,7 @@ module Directory
 
 open System.IO
 
-let deleteWithRetry path =
-    let rec fn attempts = async {
-        try
-            Directory.Delete(path, true)
-        with
-            | :? IOException when attempts > 0 ->
-                do! Async.Sleep 500
-                do! fn (attempts - 1)
-            | e -> raise e
-    }
-    fn 10
+// see https://stackoverflow.com/a/16321356/1293659
+let delete path =
+    Directory.GetFiles(path, "*", SearchOption.AllDirectories) |> Seq.iter File.Delete
+    Directory.Delete(path, true)
