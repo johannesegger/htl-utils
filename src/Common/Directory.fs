d447328820721f3a5468f9190d6cc1c420b36028
johannesegger <j.egger@posteo.at>
2020-09-22 17:11:23 +0200

    Remove read-only attributes of files and folders before deleting them

--- src/Common/Directory.fs
+++ src/Common/Directory.fs
@@ -7,6 +7,6 @@ let delete path =
         // Remove read-only attributes of files and folders
         Directory.GetDirectories(path, "*", SearchOption.AllDirectories) |> Seq.iter (fun path -> File.SetAttributes(path, FileAttributes.Normal))
         Directory.GetFiles(path, "*", SearchOption.AllDirectories) |> Seq.iter (fun path -> File.SetAttributes(path, FileAttributes.Normal))
-
+        File.SetAttributes(path, FileAttributes.Normal)
         Directory.Delete(path, true)
     with :? DirectoryNotFoundException -> ()
