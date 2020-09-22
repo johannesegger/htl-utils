7a6ea1f183575170626e198b17b16824289c605f
johannesegger <j.egger@posteo.at>
2020-09-22 16:12:05 +0200

    Increase timeout between retries when deleting directory

--- src/Common/Directory.fs
+++ src/Common/Directory.fs
@@ -8,7 +8,7 @@ let deleteWithRetry path =
             Directory.Delete(path, true)
         with
             | :? IOException when attempts > 0 ->
-                do! Async.Sleep 100
+                do! Async.Sleep 500
                 do! fn (attempts - 1)
             | e -> raise e
     }
