b2703dad06c003e824574c5a8d3289f7f0c71417
johannesegger <j.egger@posteo.at>
2020-09-24 20:46:46 +0200

    FinalTheses: Use usual parse method

--- src/FinalTheses/Core.fs
+++ src/FinalTheses/Core.fs
@@ -10,8 +10,8 @@ type private Mentors = CsvProvider<MentorsDataPath, Separators = ";">
 
 let getMentors () =
     Environment.getEnvVarOrFail "FINAL_THESES_MENTORS_FILE_PATH"
-    |> File.ReadAllText
-    |> Mentors.ParseRows
+    |> Mentors.Load
+    |> fun v -> v.Rows
     |> Seq.filter (fun row -> CIString row.Typ = CIString "Betreuer" && CIString row.Status = CIString "Aktiv")
     |> Seq.map (fun row -> { FirstName = row.Vorname.Trim(); LastName = row.Nachname.Trim(); MailAddress = row.Email.Trim() })
     |> Seq.toList
