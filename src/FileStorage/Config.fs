6433cf863c315a2d4f1c1b715dbb7b5752b41598
johannesegger <j.egger@posteo.at>
2020-11-22 20:16:28 +0100

    Make FILE_STORAGE_BASE_DIRECTORIES optional

--- src/FileStorage/Config.fs
+++ src/FileStorage/Config.fs
@@ -7,9 +7,13 @@ module Config =
     let fromEnvironment () =
         {
             BaseDirectories =
-                Environment.getEnvVarOrFail "FILE_STORAGE_BASE_DIRECTORIES"
-                |> String.split ";"
-                |> Seq.chunkBySize 2
-                |> Seq.map (fun s -> s.[0], s.[1])
-                |> Map.ofSeq
+                Environment.getEnvVar "FILE_STORAGE_BASE_DIRECTORIES"
+                |> Option.ofObj
+                |> Option.map (
+                    String.split ";"
+                    >> Seq.chunkBySize 2
+                    >> Seq.map (fun s -> s.[0], s.[1])
+                    >> Map.ofSeq
+                )
+                |> Option.defaultValue Map.empty
         }
