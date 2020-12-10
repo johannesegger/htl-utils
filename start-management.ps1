5b021efa382315021e3ee88ea3f7513bb862c8a3
johannesegger <j.egger@posteo.at>
2020-12-10 19:26:38 +0100

    Restore dotnet tools before using fable

--- start-management.ps1
+++ start-management.ps1
@@ -3,6 +3,8 @@ $serverDir = "$PSScriptRoot\src\Management.Server"
 $clientDir = "$PSScriptRoot\src\Management.Client"
 yarn --cwd $clientDir install --frozen-lockfile
 
+dotnet tool restore
+
 wt `
     new-tab --title Management.Server --startingDirectory $serverDir `-`- dotnet watch run --urls=http://+:3001`; `
     new-tab --title Management.Client --startingDirectory $clientDir `-`- dotnet fable watch .\src --run webpack-dev-server
