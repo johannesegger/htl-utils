cd5aefd5255f49733f27bef36a48bd6a73b7ff80
Johannes Egger <j.egger@posteo.at>
2023-03-17 14:19:58 +0100

    Add utility to create AD users

---
+++ utils/CreateADUsers/Program.fs
@@ -0,0 +1,25 @@
+﻿open AD
+
+let adConfig = Config.fromEnvironment ()
+let adApi = ADApi(adConfig)
+
+let userType = "Physicists" |> GroupName |> Student
+let users =
+    [
+        "Albert", "Einstein", "A.Einstein"
+    ]
+    |> List.map (fun (firstName, lastName, userName) ->
+        {
+            Name = UserName userName
+            SokratesId = None
+            FirstName = firstName
+            LastName = lastName
+            Type = userType
+        }
+    )
+let mailAliases = []
+let password = "A!b2C3"
+adApi.ApplyDirectoryModifications [
+    yield CreateGroup userType
+    for user in users -> CreateUser (user, mailAliases, password)
+]
