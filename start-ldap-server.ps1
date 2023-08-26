28c3d28fb8eb4df71e6c246893c3ac9cf9aab971
Johannes Egger <j.egger@posteo.at>
2023-08-26 06:48:38 +0200

    Remove script to set up non-Windows LDAP server

--- start-ldap-server.ps1
+++
@@ -1,8 +0,0 @@
-docker run `
-    --rm -it `
-    -p 389:389 `
-    -p 636:636 `
-    -e "SAMBA_DOMAIN=schule" `
-    -e "SAMBA_REALM=schule.intern" `
-    -e "LDAP_ALLOW_INSECURE=true" `
-    javanile/samba-ad-dc
