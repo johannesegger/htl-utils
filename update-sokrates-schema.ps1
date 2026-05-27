0b76545881e6fa7015ef57cffb66eb7b8fa989c8
Johannes Egger <eggj@htlvb.at>
2026-05-27 11:45:39 +0200

    Update Sokrates API

--- update-sokrates-schema.ps1
+++ update-sokrates-schema.ps1
@@ -1,4 +1,7 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 $Config = dotnet user-secrets list --id htl-utils --json | ConvertFrom-Json
 
 $ClientCertificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($Config.'Sokrates:ClientCertificatePath', $Config.'Sokrates:ClientCertificatePassphrase')
 Invoke-WebRequest -Certificate $ClientCertificate -Uri "$($Config.'Sokrates:WebServiceUrl')?xsd=1" -OutFile ./src/Sokrates/sokrates.xsd
+Invoke-WebRequest -Certificate $ClientCertificate -Uri "$($Config.'Sokrates:WebServiceUrl')?wsdl" -OutFile ./src/Sokrates/sokrates.wsdl
